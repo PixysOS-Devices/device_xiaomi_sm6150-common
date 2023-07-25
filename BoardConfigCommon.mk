19d3a22d829bb9c1fc95e92fb2ec5458167b44db
Shashank Patil <9945shashank@gmail.com>
2023-07-25 23:15:19 +0530

    sm6150-common: compile kernel with AOSP clang version 17.0.4
    Signed-off-by: Shashank Patil <9945shashank@gmail.com>
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,6 +78,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
+TARGET_KERNEL_CLANG_VERSION := r498229b
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
